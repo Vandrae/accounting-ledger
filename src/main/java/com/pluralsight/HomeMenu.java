61df6d026381a5cf655fc85ce91bfca6bbafa3ee
Gabe <Carrollgbusiness@gmail.com>
2026-07-14 11:13:46 -0500

    move homeMenu() to HomeMenu

--- src/main/java/com/pluralsight/HomeMenu.java
+++ src/main/java/com/pluralsight/HomeMenu.java
@@ -1,4 +1,38 @@
 package com.pluralsight;
 
 public class HomeMenu {
+
+    //First menu user sees
+    public static void homeMenu() {
+        boolean appRunning = true;
+        while (appRunning) {
+            //Display Home Screen
+            System.out.println(" ");
+            System.out.println("Home Screen");
+            System.out.println(" ");
+            System.out.println("D) Add Deposit");
+            System.out.println("P) Make Payment");
+            System.out.println("L) Ledger");
+            System.out.println("X) Exit");
+            System.out.print("Pick an option from the menu above: ");
+            String menuSelection = input.nextLine().toUpperCase();
+            System.out.println("-------------------------------------");
+            System.out.println(" ");
+
+            switch(menuSelection){
+                case "D" -> makeDeposit();
+                case "P" -> makePayment();
+                case "L" -> ledgerMenu();
+                case "X" -> {
+                    System.out.println("Thank you! Goodbye");
+                    appRunning = false;
+                }
+            }
+
+        }
+
+    }
+
+
+
 }

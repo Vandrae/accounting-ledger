330ca0293f5723487337bedd9a07c1e7725a08ee
Gabe <Carrollgbusiness@gmail.com>
2026-07-14 11:15:02 -0500

    Delete homeMenu() from AccountingApp

--- src/main/java/com/pluralsight/AccountingApp.java
+++ src/main/java/com/pluralsight/AccountingApp.java
@@ -24,36 +24,7 @@ public class AccountingApp {
         homeMenu();
     }
 
-    //First menu user sees
-    public static void homeMenu() {
-        boolean appRunning = true;
-        while (appRunning) {
-            //Display Home Screen
-            System.out.println(" ");
-            System.out.println("Home Screen");
-            System.out.println(" ");
-            System.out.println("D) Add Deposit");
-            System.out.println("P) Make Payment");
-            System.out.println("L) Ledger");
-            System.out.println("X) Exit");
-            System.out.print("Pick an option from the menu above: ");
-            String menuSelection = input.nextLine().toUpperCase();
-            System.out.println("-------------------------------------");
-            System.out.println(" ");
 
-            switch(menuSelection){
-                case "D" -> makeDeposit();
-                case "P" -> makePayment();
-                case "L" -> ledgerMenu();
-                case "X" -> {
-                    System.out.println("Thank you! Goodbye");
-                    appRunning = false;
-                }
-            }
-
-        }
-
-    }
 
     //method to be used in the home menu
     public static void makePayment() {

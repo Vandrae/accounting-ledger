49b53940c60d127e817bf2777951e555a30406d0
Chris Bunny <christopherjbunny@gmail.com>
2026-07-14 22:19:15 -0500

    Added spring dependencies and a new main to separate the cli and spring

--- src/main/java/com/pluralsight/CLIAccountingApp.java
+++ src/main/java/com/pluralsight/CLIAccountingApp.java
@@ -2,7 +2,7 @@ package com.pluralsight;
 
 import static com.pluralsight.menus.HomeMenu.displayHomeMenu;
 
-public class AccountingApp {
+public class CLIAccountingApp {
 
     public static void main(String[] args) {
 

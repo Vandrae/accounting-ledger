5e03c2b0a1955c3c5a37104c55ad630698250a7e
edomg <edomgezahegn54@gmail.com>
2026-07-14 22:01:19 -0500

    decorate ledger menu and tables

--- src/main/java/com/pluralsight/menus/LedgerMenu.java
+++ src/main/java/com/pluralsight/menus/LedgerMenu.java
@@ -1,6 +1,7 @@
 package com.pluralsight.menus;
 
 import com.pluralsight.model.Transaction;
+import com.pluralsight.util.ConsoleDecorator;
 import com.pluralsight.util.FileManager;
 
 import java.util.ArrayList;
@@ -18,30 +19,54 @@ public class LedgerMenu {
 
             ArrayList<Transaction> transactions = FileManager.loadTransactionsSortedDesc();
 
-            System.out.println(" ");
-            System.out.println("Ledger Menu");
-            System.out.println(" ");
-            System.out.println("A) All");
-            System.out.println("D) Deposits");
-            System.out.println("P) Payments");
-            System.out.println("R) Reports");
-            System.out.println("H) Home");
-            System.out.print("Pick an option from the menu above: ");
+            ConsoleDecorator.dashboard(transactions);
+            ConsoleDecorator.menu("Ledger Menu", new String[]{
+                    "A) All Transactions",
+                    "D) Deposits Only",
+                    "P) Payments Only",
+                    "R) Reports",
+                    "H) Home"
+            });
+
+            ConsoleDecorator.prompt("Choose an option");
             String selection = input.nextLine().toUpperCase();
-            System.out.println("-------------------------------------");
             System.out.println(" ");
 
             switch (selection) {
-                case "A" -> transactions.forEach(System.out::println);
-                case "D" -> transactions.stream().filter(t -> t.getAmount() > 0).forEach(System.out::println);
-                case "P" -> transactions.stream().filter(t -> t.getAmount() < 0).forEach(System.out::println);
+                case "A" -> ConsoleDecorator.transactionTable("All Transactions", transactions);
+                case "D" -> ConsoleDecorator.transactionTable("Deposits", getDeposits(transactions));
+                case "P" -> ConsoleDecorator.transactionTable("Payments", getPayments(transactions));
                 case "R" -> ReportsMenu.displayReportMenu();
                 case "H" -> {
-                    System.out.println("Returning to the main menu...");
+                    ConsoleDecorator.notice("Returning to the main menu...");
                     inLedgerMenu = false;
                 }
-                default -> System.out.println("Not a valid option.");
+                default -> ConsoleDecorator.notice("Not a valid option.");
             }
         }
+    }
+
+    private static ArrayList<Transaction> getDeposits(ArrayList<Transaction> transactions) {
+        ArrayList<Transaction> deposits = new ArrayList<>();
+
+        for (Transaction transaction : transactions) {
+            if (transaction.getAmount() > 0) {
+                deposits.add(transaction);
+            }
+        }
+
+        return deposits;
+    }
+
+    private static ArrayList<Transaction> getPayments(ArrayList<Transaction> transactions) {
+        ArrayList<Transaction> payments = new ArrayList<>();
+
+        for (Transaction transaction : transactions) {
+            if (transaction.getAmount() < 0) {
+                payments.add(transaction);
+            }
+        }
+
+        return payments;
     }
 }
